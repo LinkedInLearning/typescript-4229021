73612fbbb2815f4b11919aa50a954c7adfbb8ff0
mahedes <mahevadessart@gmail.com>
2025-01-18 00:43:06 +0100

    02_05

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,11 @@
-// Le type any
+// Le type array
 
-let nbr: any = 7;
-nbr = "abc";
+let listeA: number[] = [1, 2, 3]
+let listeB: Array<number> = [4, 5, 6]
 
+let listeC: string[] = ["a", "b", "c"]
+let listeD: Array<string> = ["d", "e", "f"]
 
-// Le type unknown
+let listeE: (string | number)[] = ["Pomme", 2, "Orange", 3, 4, "Banane"]
+let listeF: Array<string | number> = ["Pomme", 2, "Orange", 3, 4, "Banane"]
 
-let notSure: unknown = 4;
-notSure = "peut être en une string";
-notSure = false;
-
-
-// Différence entre unknown et any
-
-let vAny: any = 10;
-let vUnknown: unknown = 10;
-
-let s1: string = vAny;     // Any est attribuable à n'importe quoi
-let s2: string = vUnknown; // Invalide; Nous ne pouvons attribuer vUnknown à aucun autre type
-
-vAny.method();     // Ok; Tout fonctionne avec any
-vUnknown.method(); // Erreur; nous ne savons rien de cette variable !

80caeb871e5071def67ca7b933e8f0bb06946d86
mahedes <mahevadessart@gmail.com>
2025-01-18 00:52:39 +0100

    02_08

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-// Le type void
+// Le type never
 
-function maFonctionA(): void {
+function maFonctionA(errorMsg): never {
+    throw new Error(errorMsg);
 }
 
-function maFonctionB(): void {
-    return; // retourne undefined
-}
-
-function maFonctionC(): void {
-    return 7; // erreur
+function maFonctionB(x: string | number) {
+    if (typeof x === "string") {
+        // fait quelque chose
+    } else if (typeof x === "number") {
+        // faire quelque chose d'autre
+    } else {
+        x; // possède le type 'never'!
+    }
 }

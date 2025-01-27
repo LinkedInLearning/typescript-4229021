c4427c5086aeec62ee0e0663384056c3fef274e1
mahedes <mahevadessart@gmail.com>
2025-01-27 15:20:26 +0100

    08_05_d

---
+++ src/index.js
@@ -0,0 +1,17 @@
+class Rectangle {
+    constructor(hauteur, largeur) {
+        this.hauteur = hauteur
+        this.largeur = largeur
+    }
+
+    get area() {
+        return this.calcArea();
+    }
+
+    calcArea() {
+        return this.largeur * this.hauteur
+    }
+}
+
+const carre = new Rectangle(10, 10)
+console.log(carre.area)

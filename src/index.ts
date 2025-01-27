4c7655ed95aff301e0ca98a38b61428f3610a406
mahedes <mahevadessart@gmail.com>
2025-01-27 13:45:44 +0100

    05_07

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,15 @@
-// Les types conditionnels
+// Les types mappés
 
-// Basique
-interface Animal {
-    live(): void;
-}
-interface Car {
-    test(): void;
-}
-interface Dog extends Animal {
-    woof(): void;
-}
+type Person = {
+    name: string;
+    age: number;
+};
 
-type Example1 = Dog extends Animal ? number : string;
-type Example2 = Car extends Animal ? number : string;
+type Booleanify<T> = {
+    [P in keyof T]: boolean;
+};
 
-
-
-// Générique
-type IsNumArray<T> = T extends number[] ? number : string;
-const numA: IsNumArray<number[]> = 5;
-const numB: IsNumArray<string[]> = '5';
-
-
-
-// Contraintes de types conditionnels
-type ConditionalType<T extends number | string> = T extends number ? number : string;
-
-let x: ConditionalType<number> = 10;
-let y: ConditionalType<string> = 'Hello';
-let z: ConditionalType<boolean> = true;
-
-
-
-// Déduction dans les types conditionnels
-type Box<T> = T extends Array<infer Item> ? Item : never;
-type Books = Box<string[]>
+const boolPerson: Booleanify<Person> = {
+    name: true,
+    age: true,
+};

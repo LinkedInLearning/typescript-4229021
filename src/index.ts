d60960f9ac9ef1d582d56ab8373e6dc1385aa41b
Maheva Dessart <mahevadessart@gmail.com>
2025-01-27 15:48:24 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,8 @@ function f1(target, context) {
     console.log("Ici le decorateur f")
 }
 
-function f2(param) {
-    return function (target, context) {
-        console.log("Ici le decorateur f2 avec " + param)
-    }
-}
-
 class MaClasse {
     @f1
-    @f2("un paramètre")
     sayHello() {
         console.log("Bonjour, je suis la méthode sayHello()");
     }

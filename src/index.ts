06c61d03d3d20f988e625c787eaf7d0c8ebd5c09
mahedes <mahevadessart@gmail.com>
2025-01-27 14:36:32 +0100

    07_02

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-/// <reference path = "math.ts" /> 
+import { addition as add, soustraction } from './math'
+import *  as calcul from './math'
+import arithmetic from './math'
 
-let nombreA = Calcul.addition(3, 7)
-let nombreB = Calcul.soustraction(12, 8)
+let nombreA = add(3, 7)
+let nombreB = soustraction(12, 8)
+let nombreC = calcul.multiplication(6, 8)
+let nombreD = arithmetic.division(4, 2)
 
 console.log(nombreA)
 console.log(nombreB)
+console.log(nombreC)
+console.log(nombreD)
--- src/math.ts
+++ src/math.ts
@@ -1,8 +1,15 @@
-namespace Calcul {
-    export function addition(nbr1: number, nbr2: number) {
-        return nbr1 + nbr2;
-    }
-    export function soustraction(nbr1: number, nbr2: number) {
-        return nbr1 - nbr2;
-    }
+function addition(nbr1: number, nbr2: number) {
+    return nbr1 + nbr2;
 }
+function soustraction(nbr1: number, nbr2: number) {
+    return nbr1 - nbr2;
+}
+function multiplication(nbr1: number, nbr2: number) {
+    return nbr1 * nbr2;
+}
+function division(nbr1: number, nbr2: number) {
+    return nbr1 / nbr2;
+}
+
+export { addition, soustraction, multiplication, division }
+export default { division }

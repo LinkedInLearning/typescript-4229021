f969b1cab04d9ce8bbb63311c6adeaf52c9833ea
mahedes <mahevadessart@gmail.com>
2025-01-27 14:43:00 +0100

    07_03

---
+++ dist/index.js
@@ -0,0 +1,2 @@
+const user = { name: "Bobby", age: 30 };
+export {};
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
-import { addition as add, soustraction } from './math'
-import *  as calcul from './math'
-import arithmetic from './math'
+import type { User } from "./user";
 
-let nombreA = add(3, 7)
-let nombreB = soustraction(12, 8)
-let nombreC = calcul.multiplication(6, 8)
-let nombreD = arithmetic.division(4, 2)
-
-console.log(nombreA)
-console.log(nombreB)
-console.log(nombreC)
-console.log(nombreD)
+const user: User = { name: "Bobby", age: 30 };

dcd3b3c69ad856632609d67d856e53f911eff3e2
mahedes <mahevadessart@gmail.com>
2025-01-27 11:00:44 +0100

    03_07

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,76 @@
-// Les types littéraux
+// Partial 
+// rend les propriétés d'un objet facultatives
 
-let x: string = "Jour"
-x = "Nuit"
+interface Point {
+    x: number;
+    y: number;
+}
 
-let y: "bonjour" = "bonjour"
-y = "bonsoir"
+let pointPart: Partial<Point> = {}; // x et y sont optionnelles
+pointPart.x = 10;
 
 
-let alignment: "left" | "right" | "center";
+// Required
+// rend les propriétés d'un objet obligatoires.
 
-alignment = "right"
-alignment = "top"
+interface Car {
+    make: string;
+    model: string;
+    mileage?: number; // propriété optionnelle
+}
 
+let myCar: Required<Car> = {
+    make: 'Ford',
+    model: 'Focus',
+    mileage: 12000 // `Required` force mileage à être défini
+};
+
+
+// Omit
+// supprime les clés d'un type d'objet.
+
+interface PersonA {
+    name: string;
+    age: number;
+    location?: string;
+}
+
+const bob: Omit<PersonA, 'age' | 'location'> = {
+    name: 'Bob'
+    // `Omit` a supprimé age and location, ils ne peuvent pas être défini ici
+};
+
+
+// Pick
+// supprime toutes les clés sauf celles spécifiées d'un type d'objet.
+
+interface PersonB {
+    name: string;
+    age: number;
+    location?: string;
+}
+
+const marley: Pick<PersonB, 'name'> = {
+    name: 'Bob'
+    // `Pick` a seulement gardé name. age et location ont été supprimé
+};
+
+
+// Readonly
+// crée un nouveau type où toutes les propriétés sont en lecture seule, 
+// ce qui signifie qu'elles ne peuvent pas être modifiées une fois
+// qu'une valeur leur est attribuée.
+
+interface Employee {
+    id: number;
+    name: string;
+}
+
+let emp1: Readonly<Employee> = {
+    id: 1,
+    name: "Steve"
+}
+
+emp1.id = 100;
+emp1.name = 'Bill';
 

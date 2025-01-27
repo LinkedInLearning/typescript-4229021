fedec9b2edde31858cd319517ad2332e8958dcd9
mahedes <mahevadessart@gmail.com>
2025-01-27 13:30:06 +0100

    05_03

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
-// Le typage générique
+// L'opérateur keyof
 
-function identity<T1, T2>(arg: T1, arg2: T2): T1 {
-    return arg;
+type ContactDetails = { name: string; email: string; mobile: string };
+
+type Contact = keyof ContactDetails;
+
+const friend: Contact = "mobile"
+
+
+
+// Autre exemple
+
+interface Person {
+    name: string;
+    age: number;
 }
 
-let outputS = identity<string, number>("myString", 5);
+function showPerson(obj: Person, property: keyof Person) {
+    console.log(property + " : " + obj[property]);
+}
+
+let personA = {
+    name: "Booby",
+    age: 32
+};
+
+showPerson(personA, "name"); 

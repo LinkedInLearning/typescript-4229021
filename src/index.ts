7a82e6c8b4d5e1e19ab241a7620f3519716f6f89
mahedes <mahevadessart@gmail.com>
2025-01-27 13:43:14 +0100

    05_06

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,37 @@
-// Les types d'accès indexés
+// Les types conditionnels
 
-type Person = { age: number; name: string; alive: boolean };
-type Age = Person["age"];
+// Basique
+interface Animal {
+    live(): void;
+}
+interface Car {
+    test(): void;
+}
+interface Dog extends Animal {
+    woof(): void;
+}
 
-type AgeName = Person["age" | "name"];
+type Example1 = Dog extends Animal ? number : string;
+type Example2 = Car extends Animal ? number : string;
 
-type key = "age";
-type T = Person[key]
+
+
+// Générique
+type IsNumArray<T> = T extends number[] ? number : string;
+const numA: IsNumArray<number[]> = 5;
+const numB: IsNumArray<string[]> = '5';
+
+
+
+// Contraintes de types conditionnels
+type ConditionalType<T extends number | string> = T extends number ? number : string;
+
+let x: ConditionalType<number> = 10;
+let y: ConditionalType<string> = 'Hello';
+let z: ConditionalType<boolean> = true;
+
+
+
+// Déduction dans les types conditionnels
+type Box<T> = T extends Array<infer Item> ? Item : never;
+type Books = Box<string[]>

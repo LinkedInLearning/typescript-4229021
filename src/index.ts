91306b3b0841d6b79d4ce4fcfa31876b429a82d3
mahedes <mahevadessart@gmail.com>
2025-01-27 11:44:26 +0100

    04_01

--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,27 @@
-// Partial 
-// rend les propriétés d'un objet facultatives
+// Typage de fonctions
 
-interface Point {
-    x: number;
-    y: number;
+function calculA(x, y) {
+    return x + y;
 }
-
-let pointPart: Partial<Point> = {}; // x et y sont optionnelles
-pointPart.x = 10;
+calculA(2, 3);
 
 
-// Required
-// rend les propriétés d'un objet obligatoires.
 
-interface Car {
-    make: string;
-    model: string;
-    mileage?: number; // propriété optionnelle
+function calculB(x, y?) {
+    return x + y;
 }
-
-let myCar: Required<Car> = {
-    make: 'Ford',
-    model: 'Focus',
-    mileage: 12000 // `Required` force mileage à être défini
-};
+calculB(2);
 
 
-// Omit
-// supprime les clés d'un type d'objet.
 
-interface PersonA {
-    name: string;
-    age: number;
-    location?: string;
+function calculC(x, y = 1) {
+    return x + y;
 }
-
-const bob: Omit<PersonA, 'age' | 'location'> = {
-    name: 'Bob'
-    // `Omit` a supprimé age and location, ils ne peuvent pas être défini ici
-};
+calculC(2, 3);
 
 
-// Pick
-// supprime toutes les clés sauf celles spécifiées d'un type d'objet.
 
-interface PersonB {
-    name: string;
-    age: number;
-    location?: string;
+function calculD(x: string, y: number): string {
+    return x + y;
 }
-
-const marley: Pick<PersonB, 'name'> = {
-    name: 'Bob'
-    // `Pick` a seulement gardé name. age et location ont été supprimé
-};
-
-
-// Readonly
-// crée un nouveau type où toutes les propriétés sont en lecture seule, 
-// ce qui signifie qu'elles ne peuvent pas être modifiées une fois
-// qu'une valeur leur est attribuée.
-
-interface Employee {
-    id: number;
-    name: string;
-}
-
-let emp1: Readonly<Employee> = {
-    id: 1,
-    name: "Steve"
-}
-
-emp1.id = 100;
-emp1.name = 'Bill';
-
+calculD("abc", 3);

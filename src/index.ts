7021c6e3c9e7f80f35fb626282c380e4b8629041
mahedes <mahevadessart@gmail.com>
2025-01-27 13:48:03 +0100

    05_08

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
-// Les types mappés
+// Les types littéraux de modèle
 
-type Person = {
-    name: string;
-    age: number;
-};
+// Exemple 1
+type World = "world";
+type Greeting = `hello ${World}`;
 
-type Booleanify<T> = {
-    [P in keyof T]: boolean;
-};
 
-const boolPerson: Booleanify<Person> = {
-    name: true,
-    age: true,
-};
+// Exemple 2
+type EmailLocaleIDs = "welcome_email" | "email_heading";
+type FooterLocaleIDs = "footer_title" | "footer_sendoff";
+type AllLocaleIDs = `${EmailLocaleIDs | FooterLocaleIDs}_id`;
+
+
+// Exemple 3
+type Size = "small" | "medium" | "large";
+type ResponseMessage = `${Size} size selected`;
+
+
+// Exemple 4
+type Lang = "en" | "ja" | "pt";
+type LocaleMessageIDs = `${Lang}_${AllLocaleIDs}`;
+
